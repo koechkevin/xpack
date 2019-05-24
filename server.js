a62914c03063e0b9b5618e3e45ae99332c67209e
koechkevin <koechkevin92@gmail.com>
2019-05-24 20:08:51 +0300

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.port||5000;
+const port = process.env.PORT||5000;
 const app = express();
 
 const DIST = path.resolve(__dirname, 'dist');
